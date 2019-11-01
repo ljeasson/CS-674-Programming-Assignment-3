9454e32d8915338bfc3d7c84dd304295882e4e65
Lee Easson <ljeasson@gmail.com>
2019-11-01 13:07:36 -0700

    Add plot for Rect_128.dat

--- dft_exp1.py
+++ dft_exp1.py
@@ -1,13 +1,15 @@
+import math
+import numpy as np
 import matplotlib.pyplot as plt
 import ctypes
 
 # Initialize fft function from fft.c
-def fourier_transform(f, nn, isign):
-    fun = ctypes.CDLL(fft.so)   
-    fun.fft.argtypes(ctypes.c_arr, ctypes.c_long, ctypes.c_int) # Check if c_arr is a thing
+#def fourier_transform(f, nn, isign):
+#    fun = ctypes.CDLL(fft.so)   
+#    fun.fft.argtypes(ctypes.c_arr, ctypes.c_long, ctypes.c_int) # Check if c_arr is a thing
 
     # Call fft
-    fun.fft(f, nn, 1)
+#    fun.fft(f, nn, 1)
 
 ###################################
 # Experiment 1.a
@@ -15,7 +17,7 @@ f = [2,3,4,4] # Input Signal
 nn = 4 # Must be power of 2
 
 # Apply fourier transform to signal
-fourier_transform(f, nn, 1)
+#fourier_transform(f, nn, 1)
 
 ###################################
 # Experiment 1.b
@@ -28,3 +30,17 @@ N = 128
 file = open("Rect_128.dat", "r") 
 read_file = file.read()
 rect_data = read_file.split("\n")
+x = []
+y = []
+for i in range(len(rect_data)):
+    x.append(i)
+    y.append(rect_data[i])
+
+
+plt.plot(x, y, 'ro')
+plt.axis([0, 128, 0, 2])
+
+plt.title("Rect_128.dat")
+plt.xlabel("Real (X)")
+plt.ylabel("Imaginary (Y)")
+plt.show()
